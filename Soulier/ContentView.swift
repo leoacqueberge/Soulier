9dbbe51ace3a9e47ecce851dfa3d07b657672e71
Leo Acqueberge <leoacqueberge@icloud.com>
2026-07-03 23:05:20 +0200

    Initial Commit

---
+++ Soulier/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Soulier
+//
+//  Created by Léo  Acqueberge  on 7/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Soulier/SoulierApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoulierApp.swift
+//  Soulier
+//
+//  Created by Léo  Acqueberge  on 7/3/26.
+//
+
+import SwiftUI
+
+@main
+struct SoulierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
